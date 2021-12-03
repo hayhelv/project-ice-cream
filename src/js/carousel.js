c9c1bdf814dd49efedc5d028ed8de30440cbff32
llomaka <llomaka80@gmail.com>
2021-12-03 22:37:30 +0200

    Слайдер додано, не налаштовано піктограми пагінації

---
+++ src/js/carousel.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+  dots: true,
+  dotsClass: "customer-reviews__dots",
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  fade: true,
+  cssEase: 'linear'
+});
