a99758e3620655c1d04acb4df60d907373430c1f
Kseniia Malyshenko <kseniyamesch0101@gmail.com>
2021-12-07 13:47:18 +0200

    Read more

---
+++ src/js/modal-read-more.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modalReadMore-open]'),
+      closeModalBtn: document.querySelector('[data-modalReadMore-close]'),
+      modal: document.querySelector('[data-modalReadMore]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('overlay--hidden');
+    }
+  })();
+  
